c0019e0a34586c1526942c3f96dc566f5b53cf1c
Vamsi_Poddidi <vamsida70@gmail.com>
2025-11-10 19:13:17 +0530

    Delete teluguupdates directory

--- teluguupdates/passwordgenerator.php
+++
@@ -1,9 +0,0 @@
-<?php
-// --- Auto-init to prevent undefined variable warnings ---
-$cats = $cats ?? [];
-$posts = $posts ?? [];
-$trend = $trend ?? [];
-$cstmt = $cstmt ?? [];
-$all_categories = $all_categories ?? [];
-
-echo password_hash("Admin#2025", PASSWORD_DEFAULT);
